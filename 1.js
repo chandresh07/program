c1140fe293a022b47aa6ae17cfdf349c0e196989
chandresh rathod <chandreshrathod9465@gmail.com>
2023-08-01 00:57:06 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,13 @@
+function myAlert(msg){
+    var a=prompt("enter number!");
+    if(a%== 0)
+    {
+        alert("number is even"+a);
+    }
+    else{
+        alert("number is odd"+a);
+    }
+   // var b=prompt("enter name");
+  //  var c=prompt("enter your password");
+//alert("login successful"+a,+b);
+}
